d8d90ea0a7fa58cd8d23b6e9fb78603d7aefbae9
BahdVec <nerdydesigns00@gmail.com>
2022-09-08 22:23:28 +0100

    Size of byte

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long long int d;
 float e;
 
 
-printf("Size of char: %zu byte\n", sizeof(a));
-printf("Size of int: %zu byte\n", sizeof(b));
-printf("Size of long int: %zu byte\n", sizeof(c));
-printf("Size of long long int: %zu byte\n", sizeof(d));
-printf("Size of float: %zu byte\n", sizeof(e));
+printf("Size of char: %zu byte(s)\n", sizeof(a));
+printf("Size of int: %zu byte(s)\n", sizeof(b));
+printf("Size of long int: %zu byte(s)\n", sizeof(c));
+printf("Size of long long int: %zu byte(s)\n", sizeof(d));
+printf("Size of float: %zu byte(s)\n", sizeof(e));
 
 return (0);
 
