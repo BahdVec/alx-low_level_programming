385724fdfb0b9af192be80039c1af0a5f865549e
BahdVec <nerdydesigns00@gmail.com>
2022-09-09 18:49:05 +0100

    All

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char all;
+
+all = 'a';
+while (all <= 'z')
+{
+	if (char all != q && char all != e)
+	{
+	putchar(all);
+	all++
+	}
+}
+	putchar('\n');
+	return (0);
+}
