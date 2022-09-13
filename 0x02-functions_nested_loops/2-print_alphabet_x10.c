ffc11671215e9a1bd7a6704b923221c453841e87
BahdVec <nerdydesigns00@gmail.com>
2022-09-13 17:34:56 +0100

    Printing Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a, b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(c);
+		}
+		_putchar("\n");
+	}
+}
