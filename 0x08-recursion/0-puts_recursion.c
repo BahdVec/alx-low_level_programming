5f34c5f1bbdc15b8235d86cacd2992ec02024a92
BahdVec <nerdydesigns00@gmail.com>
2022-09-28 16:35:24 +0100

    Recursion(Puts)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,11 +11,10 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s++);
+		_puts_recursion(s + 1);
 	}
-	else if (*s == '\0')
+	else
 	{
 		_putchar('\n');
-		return;
 	}
 }
