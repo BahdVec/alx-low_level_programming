b052f3d7a476607a873a26d0d93d1a62add5fa71
BahdVec <nerdydesigns00@gmail.com>
2022-09-30 19:24:49 +0100

    Argc, Argv(mul)

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply 2 numbers passed to main, or Error
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if error, 0 if function runs correctly
+ */
+int main(int argc, char *argv[])
+{
+	int result = 1, i;
+
+	if (argc != 3)
+	{
+		printf("Error");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int x = strtol(argv[i], NULL, 10);
+
+			result = result * x;
+		}
+		printf("%d\n", result);
+	}
+}
