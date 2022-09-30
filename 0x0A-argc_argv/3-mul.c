c5969455c08367aa2092391a853ce036f226072f
BahdVec <nerdydesigns00@gmail.com>
2022-09-30 19:31:28 +0100

    Argc, Argv(mul)

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,25 +8,18 @@
  * @argv: argument vector
  * Return: 1 if error, 0 if function runs correctly
  */
+
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	int result = 1, i;
 
-	if (argc != 3)
+	if (argv[1] && argv[2])
 	{
-		printf("Error\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			int x = strtol(argv[i], NULL, 10);
-
-			result = result * x;
-		}
-		printf("%d\n", result);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
+	else
+		printf("Error\n");
+
 	return (1);
 }
