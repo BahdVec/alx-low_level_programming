14b797caa9d1b35c62da16593e4cc8a9b9fc2a70
BahdVec <nerdydesigns00@gmail.com>
2022-10-10 16:44:12 +0100

    Struct(Print Struct using Struct pointer)

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
+
+	printf("Name: %s", (*d).name);
+	printf("\nAge: %f", (*d).age);
+	printf("\nOwner: %s", (*d).owner);
+}
