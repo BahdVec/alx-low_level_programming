f1a649b74e87a90f7eb3d9ac59cb2eba7f1d2285
BahdVec <nerdydesigns00@gmail.com>
2022-10-13 20:11:21 +0100

    Variadic function (Sum)

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,7 +10,8 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i = 0, sum = 0, x;
+	unsigned int i = 0;
+	int sum = 0;
 	va_list args;
 
 	va_start(args, n);
@@ -23,7 +24,7 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		for (i = 0; i < n; i++)
 		{
-			x = va_arg(args, int);
+			int x = va_arg(args, int);
 			sum = sum + x;
 		}
 	}
