3b51a9e545ce8b2459ad77c9feeeb989f796d5b0
BahdVec <nerdydesigns00@gmail.com>
2022-10-27 20:34:14 +0100

    Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - a func that the value of a bit at any given index
+ * @n: an integer
+ * @index: given index
+ * Return: the value at index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	return ((n >> index) & 1);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+<h1>C- Bit Manipulation</h1>
+<p>Just another readme</p>
